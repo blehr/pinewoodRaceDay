101bcb62b634f49a5118298206e34c4279fc6212
blehr <blehr.mail@gmail.com>
2016-05-31 16:13:15 -0400

    update footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -5,9 +5,11 @@ const Footer = () => {
   return (
     <div className="row" >
       <footer>
-        <div className="col-sm-4"></div>
         <div className="col-sm-4 text-center">
-          <p style={{'marginTop': '30px'}}>&copy; 2016 <a href="http://brandonlehr.com" target="_blank">Brandon Lehr</a></p>
+          <p><a href="https://github.com/blehr/pinewoodRaceDay" >View on GitHub <span className="fa fa-github"></span></a></p>
+        </div>
+        <div className="col-sm-4 text-center">
+          <p>&copy; 2016 <a href="http://brandonlehr.com" target="_blank">Brandon Lehr</a></p>
         </div>
         <div className="col-sm-4"></div>
       </footer>
